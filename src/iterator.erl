cda8ceff923984a12928d90c3595be4bce47aa82
Eric Moritz <eric@themoritzfamily.com>
2013-01-02 15:54:03 -0500

    Added a simple iterator function

---
+++ src/iterator.erl
@@ -0,0 +1,29 @@
+% -*- erlang -*-
+-module(iterator).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+iterate(_, {Accum, done}) ->
+    Accum;
+iterate(Callback, {Accum, Current}) ->
+    iterate(
+      Callback,
+      Callback(Accum, Current)
+     ).
+
+-ifdef(TEST).
+iterate_test_() ->
+    ?_assertEqual(
+       [1,2,3,4,5],
+       iterate(
+         fun(Accum, 0) ->
+                 {Accum, done};
+            (Accum, Counter) ->
+                 {[Counter|Accum], Counter - 1}
+         end,
+         {[], 5}
+        )
+      ).
+-endif.
